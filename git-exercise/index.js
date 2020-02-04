939ff773d0ba85f355aa762cfaa3bcbaf588c0e2
juullasse <lassejuul@live.com>
2020-02-04 11:00:44 +0100

    working bubbles

--- git-exercise/index.js
+++ git-exercise/index.js
@@ -1,13 +1,13 @@
-window.addEventListener("load")
+window.addEventListener("load", start)
 
 function start(){
-    const bubbles = document.querySelectorAll("wrapper>div");
+    const bubbles = document.querySelectorAll("#wrapper>div");
     console.log(bubbles)
     bubbles.forEach(bubble=> {
         const x = Math.random()*100;
         const y = Math.random()*100;
-        const hue = Math.random()*360;
-        bubble.style.background = "hsl(120, 70%, 70%)"
-        bubble.style.transform="translate("+x+"vw, "+y"vh)";
+        /*const hue = Math.random()*360;
+        bubble.style.background = "hsl(120, 70%, 70%)"*/
+        bubble.style.transform="translate("+x+"vw, "+y+"vh)";
     })
 }
